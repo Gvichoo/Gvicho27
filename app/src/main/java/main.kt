70330fb6bc1d5ce5b47e86267002dbefabe4ba66
Gvichoo <saba.gvichiani27@gmail.com>
2024-11-27 19:54:35 +0400

    finished project

--- app/src/main/java/main.kt
+++ app/src/main/java/main.kt
@@ -1,29 +1,35 @@
-fun main(){
-    var x  = readln()
-    var y  = readln()
+fun main() {
+    var x = readln()
+    var y = readln()
     var numbersOnlyX = x.filter { it.isDigit() }
-    var numbersOnlyY = y.filter{ it.isDigit() }
-    if (numbersOnlyX.isEmpty() || numbersOnlyY.isEmpty()) {
+    var numbersOnlyY = y.filter { it.isDigit() }
+    if (numbersOnlyX.isEmpty()) {
         numbersOnlyX = "0"
+        numbersOnlyX.toInt()
+    }
+    if (numbersOnlyY.isEmpty()) {
         numbersOnlyY = "0"
-        var numX = numbersOnlyX.toInt()
-        var numY = numbersOnlyY.toInt()
-        var z: Int = numX / numY
-        println("X და Y განაყოფი არის: $z")
+        numbersOnlyY.toInt()
+    }
+    var numX = numbersOnlyX.toDouble()
+    var numY = numbersOnlyY.toDouble()
+    var z = numX / numY
+    if (z.isNaN()){
+        println("პასუხი არაფერია")
     }else{
-        var numX = numbersOnlyX.toDouble()
-        var numY = numbersOnlyY.toDouble()
-        var z: Double = numX / numY
         println("X და Y განაყოფი არის: $z")
     }
+
     println("გსურთ ახალი პროექტის დაწყება<Y/N>?")
     val ANSWER = readln()
-    if (ANSWER == "დიახ"){
+    if (ANSWER == "დიახ") {
         println("ხელახლა დაწყება")
         main()
-    }else if(ANSWER == "არა"){
+    } else if (ANSWER == "არა") {
         println("დასასრული")
-    }else{
+    } else {
         println("პასუხი არასწორად აკრიფეთ.")
+        main()
     }
 }
+
